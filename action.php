f3ddf31860cd45bdb8a3fdf986adf01843aa4087
magpiapple <aaron.y.lim@gmail.com>
2021-12-17 17:37:47 -0800

    created database and some php

--- action.php
+++ action.php
@@ -1,1 +1,36 @@
-<h1>php page</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+<form action= "action.php" form method="studioghib">
+    search: <input type="varchar" name="age"><input type = "submit">
+
+
+<label>Search</label>
+<input type="text" name="search">
+
+</form>
+
+</body>
+
+</html>
+<?php
+    require_once "action2.php"
+
+try {
+  $conn = new PDO("mysql:host=$servername", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "CREATE DATABASE myDBPDO";
+  $conn->exec($sql);
+  echo "Database created successfully<br>";
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+$query = "select distinct movieTitle from studioghib where maincharacters like '" . $_POST['cName] ."';";
+
+$conn = null;
+?>
---
+++ action2.php
@@ -0,0 +1,7 @@
+    <?php
+        $host = "127.0.0.1";
+        $user = "root";
+        $pass = "";
+        $db = "ghib";
+     $port = 3307;
+    ?>

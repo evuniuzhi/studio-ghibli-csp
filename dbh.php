2171914ebfd679a872a3d5b1a6b18c037e7e9aea
evuniuzhi <89225978+evuniuzhi@users.noreply.github.com>
2021-12-21 17:08:37 -0800

    updated port

--- dbh.php
+++ dbh.php
@@ -3,7 +3,7 @@
     $user = "be3cdb8a15f7f0";
     $pass = "400a94ec";
     $dbname = "heroku_64b1190769a8cb3";
-    $port = "";
+    //$port = "";
 
     $conn = mysqli_connect($host, $user, $pass, $dbname);
 ?>

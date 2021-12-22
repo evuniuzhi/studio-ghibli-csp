e4875c085843d36dff275ff2deb5bc86253c2079
evuniuzhi <89225978+evuniuzhi@users.noreply.github.com>
2021-12-21 16:56:47 -0800

    connected to heroku database

--- dbh.php
+++ dbh.php
@@ -1,10 +1,9 @@
 <?php
-    $host = "localhost";
-    $user = "root";
-    $pass = "";
-    $dbname = "ghib";
-    $port = "3306";
+    $host = "us-cdbr-east-05.cleardb.net";
+    $user = "be3cdb8a15f7f0";
+    $pass = "400a94ec";
+    $dbname = "heroku_64b1190769a8cb3";
+    $port = "";
 
-    $conn = mysqli_connect($host, $user, $pass, $dbname, $port);
+    $conn = mysqli_connect($host, $user, $pass, $dbname);
 ?>
-
